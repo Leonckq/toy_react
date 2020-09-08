6f7b38f87364c6ba799c0956723c07199d4c0394
liyang <847450261@qq.com>
2020-09-08 08:41:09 +0800

    环境搭建

---
+++ main.js
@@ -0,0 +1,6 @@
+for(let a of [1,2,3,4]) {
+  console.log(a)
+}
+
+let a = <div/>
+console.log(a)
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+module.exports = {
+  entry: './main.js',
+  mode: 'development',
+  optimization: {
+    minimize: false
+  },
+  output: {
+    path: path.resolve('dist'),
+    filename: 'main.[contentHash:7].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/plugin-transform-react-jsx', {pragma: 'createElement'}]]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin({})
+  ],
+  devServer: {
+    // hot: true,
+    contentBase: path.join(__dirname, "dist"),
+    port: 9966
+  },
+  resolve: {
+    modules: [ path.resolve('node_modules') ],
+    extensions: ['.js', '.jsx', '.tsx', '.css', '.json']
+  },
+}
